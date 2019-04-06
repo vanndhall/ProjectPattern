7652ef69cba0892bc7e73568837d89fc07ef76cc
vanndhall <szymon.mantey@wp.pl>
2019-04-06 13:21:54 +0200

    BuilderPattern - in progress

---
+++ src/main/java/builder/Statistics.java
@@ -0,0 +1,15 @@
+package builder;
+
+public class Statistics {
+    private int strength;
+    private int weaponsSkill;
+    private int ballisticsSkill;
+    private int toughness;
+    private int agility;
+    private int intelligence;
+    private int willPower;
+    private int fellowship;
+    private int attacks;
+    private int sthrenghtBonus;
+    private int toughnessBonus;
+}
